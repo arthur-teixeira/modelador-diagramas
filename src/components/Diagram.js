7a157da6b5a6e403185d67bfe7ea9e3b224d2700
arthur <arthurcarvalho24@gmail.com>
2019-07-16 18:44:32 -0300

    drag and drop

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -1,33 +1,57 @@
 import {
-   DiagramEngine,
-   DiagramModel,
-   DefaultNodeModel,
    DiagramWidget,
-} from "storm-react-diagrams";
-import React from 'react'
+   DiagramEngine,
+   DefaultNodeFactory,
+   DefaultLinkFactory,
+   DefaultNodeModel,
+   DefaultPortModel
+} from 'storm-react-diagrams';
+import React, { Component } from 'react'
+import Lodash from 'lodash'
 
 import 'storm-react-diagrams/dist/style.min.css'
 
-export default () => {
-   var engine = new DiagramEngine();
-   engine.installDefaultFactories();
+class Diagram extends Component {
+   componentWillMount() {
+      this.engine = new DiagramEngine();
+      this.engine.registerNodeFactory(new DefaultNodeFactory());
+      this.engine.registerLinkFactory(new DefaultLinkFactory());
+   }
+   render() {
+      return (
+         <div className="content">
+            <div
+               className="diagram-layer"
+               onDrop={event => {
+                  var data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
+                  var nodesCount = Lodash.keys(this.engine.getDiagramModel().getNodes()).length + 1;
+                  var node = null;
+                  if (data.type === 'in') {
+                     node = new DefaultNodeModel('Node ' + (nodesCount++), '#00c0ff');
+                     node.addPort(new DefaultPortModel(true, 'in-1', 'In'));
+                  } else if (data.type === 'out') {
+                     node = new DefaultNodeModel('Node ' + (nodesCount++), '#c0ff00');
+                     node.addPort(new DefaultPortModel(false, 'out-1', 'Out'));
+                  } else if (data.type === 'in/out') {
+                     node = new DefaultNodeModel('Node ' + (nodesCount++), '#c000ff');
+                     node.addPort(new DefaultPortModel(true, 'in-1', 'In'));
+                     node.addPort(new DefaultPortModel(false, 'out-1', 'Out'));
+                  }
+                  var points = this.engine.getRelativeMousePoint(event);
+                  node.x = points.x;
+                  node.y = points.y;
+                  this.engine.getDiagramModel().addNode(node);
+                  this.forceUpdate();
+               }}
+               onDragOver={event => {
+                  event.preventDefault();
+               }}
+            >
+               <DiagramWidget className="srd-canvas" diagramEngine={this.engine} />
+            </div>
+         </div>
+      );
+   }
+}
 
-   var model = new DiagramModel();
-
-   var node1 = new DefaultNodeModel("Node 1", "rgb(0,192,255)");
-   let port1 = node1.addOutPort("Out");
-   node1.setPosition(100, 100);
-
-   var node2 = new DefaultNodeModel("Node 2", "rgb(192,255,0)");
-   let port2 = node2.addInPort("In");
-   node2.setPosition(400, 100);
-
-   let link1 = port1.link(port2);
-   link1.addLabel("Hello World!");
-
-   model.addAll(node1, node2, link1);
-
-   engine.setDiagramModel(model);
-
-   return <DiagramWidget className="srd-canvas" diagramEngine={engine} />;
-};
+export default Diagram
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import { listItem } from './styled'
+import React, { useState } from 'react'
+import TrayWidget from './tray/TrayWidget'
+import TrayItemWidget from './tray/TrayItemWidget'
 
 const SideMenu = props => {
-   const menuItems = ['node in', 'node out']
+
+   const [TrayItems] = useState(
+      [
+         {
+            name: 'Node In',
+            type: 'in'
+         },
+         {
+            name: 'Node Out',
+            type: 'out'
+         },
+         {
+            name: 'Node In/Out',
+            type: 'in/out'
+         }
+      ]
+   )
 
    return (
-      <div>
-         <ul>
-            {menuItems.map((item, index) => (
-               <li key={index}>
-                  {item}
-               </li>
-            ))}
-         </ul>
-      </div>
+      <TrayWidget>
+         {TrayItems.map((item, index) => (
+            <TrayItemWidget model={{ type: item.type }} name={item.name} key={index} />
+         ))}
+      </TrayWidget>
+
    )
 }
 
+
+
 export default SideMenu
+
