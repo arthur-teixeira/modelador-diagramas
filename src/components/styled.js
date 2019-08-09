268a4a707da69c2e9dad3ad0bb4cbe8dbf548a75
arthur <arthurcarvalho24@gmail.com>
2019-08-09 13:52:56 -0300

    novos estilos

--- src/components/styled.js
+++ src/components/styled.js
@@ -12,3 +12,26 @@ export const Container = styled.div`
    background-color: #1e1e1e;
    height:100vh;
 `
+
+export const Card = styled.div`
+   background:#3c3c3c;
+   color: white;
+   padding: 1%;
+   border-radius: 5px;
+   &:hover {
+      transform: scale(1.02);
+      transition: 0.2s;
+   }
+   
+   
+`
+
+export const GridWrapper = styled.div`
+   display:grid;
+   grid-template-columns: repeat(3, 1fr);
+   grid-gap: 1rem;
+   grid-auto-rows: minmax(10vh, auto);
+   background: #1e1e1e;
+   padding: 1rem;
+   
+`
