b9090414eeedaaceb8519b9c4b62d9aa3cb20cf7
arthur-teixeira <arthurcarvalho24@gmail.com>
2019-09-17 19:05:10 -0300

    mudancas

--- src/components/load/LoadPage.js
+++ src/components/load/LoadPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import api from '../../api/axios'
-import { Card, GridWrapper, Container } from '../styled'
+import { Card, GridWrapper, Container, Button } from '../styled'
 import LoadHeader from './LoadHeader'
 import { Link } from 'react-router-dom'
 
@@ -18,17 +18,26 @@ export default class LoadPage extends Component {
       console.log(this.state.data)
    }
 
+   handleDelete = async (id, e) => {
+      const data = await api.delete(`remover/${id}`);
+      console.log(data)
+   }
+
    render() {
       return (
          <Container>
             <LoadHeader />
             <GridWrapper>
                {this.state.data.map((item) => (
+               <>
                   <Link key={item._id} to={`/canvas/${item._id}`}>
                      <Card>
                         <h3>{item.nome}</h3>
                      </Card>
                   </Link>
+                  <Button onClick={e => this.handleDelete(item._id, e)}>Deletar</Button>
+               
+               </>
                ))}
             </GridWrapper>
          </Container>
